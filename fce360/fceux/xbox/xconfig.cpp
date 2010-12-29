1389ca66a2b331da5a2230fbdf568c0d0b13f496
Ced2911 <Ced2911@1f2d827f-b39e-40f8-2521-43ba25b7e373>
2010-12-29 14:23:14 +0000

    Added configuration for input

--- fce360/fceux/xbox/xconfig.cpp
+++ fce360/fceux/xbox/xconfig.cpp
@@ -8,9 +8,9 @@ Config fcecfg;
 
 void ReadConfig()
 {
-	/*
-	*/
-	//SetDefault Value overwritten during loading
+	/***
+	Sound
+	**/
 	fcecfg.Set("sound","enabled", 1);
 	fcecfg.Set("sound","rate", 48000);
 	fcecfg.Set("sound","bufsize", 128);
@@ -21,13 +21,65 @@ void ReadConfig()
 	fcecfg.Set("sound","noisevolume", 256);
 	fcecfg.Set("sound","pcmvolume", 256);
 
+	/***
+	Vidéo
+	**/
 	fcecfg.Set("video","region","NTSC"); //not used
 	fcecfg.Set("video","swfilter", 1);
 	fcecfg.Set("video","screenaspect", 1);
 	
+	/***
+	Controller
+	**/
+	DWORD gamepad_dpad_up, gamepad_dpad_down, gamepad_dpad_left, gamepad_dpad_right, 
+		gamepad_start, gamepad_back, 
+		gamepad_left_thumb, gamepad_right_thumb, 
+		gamepad_left_shoulder, gamepad_right_shoulder, 
+		gamepad_a, gamepad_b, gamepad_x, gamepad_y,
+		gamepad_left_trigger, gamepad_right_trigger;
+	
+	gamepad_dpad_up = JOY_UP;
+	gamepad_dpad_down = JOY_DOWN;
+	gamepad_dpad_left = JOY_LEFT;
+	gamepad_dpad_right = JOY_RIGHT;
+
+	gamepad_a = JOY_A;
+	gamepad_b = JOY_B;
+	gamepad_x = JOY_B; 
+	gamepad_y = 0;
+
+	gamepad_start = JOY_START; 
+	gamepad_back = JOY_SELECT;
+
+	gamepad_left_shoulder = 0; 
+	gamepad_right_shoulder = 0;
+
+	gamepad_left_thumb = 0; 
+	gamepad_right_thumb = 0;
+
+	gamepad_left_trigger = JOY_A;
+	gamepad_right_trigger = JOY_B;
+
+	fcecfg.Set("controller","XINPUT_GAMEPAD_DPAD_UP", gamepad_dpad_up );
+	fcecfg.Set("controller","XINPUT_GAMEPAD_DPAD_DOWN", gamepad_dpad_down );
+	fcecfg.Set("controller","XINPUT_GAMEPAD_DPAD_LEFT", gamepad_dpad_left );
+	fcecfg.Set("controller","XINPUT_GAMEPAD_DPAD_RIGHT", gamepad_dpad_right );
+	fcecfg.Set("controller","XINPUT_GAMEPAD_START", gamepad_start );
+	fcecfg.Set("controller","XINPUT_GAMEPAD_BACK", gamepad_back );
+	fcecfg.Set("controller","XINPUT_GAMEPAD_A", gamepad_a );
+	fcecfg.Set("controller","XINPUT_GAMEPAD_B", gamepad_b );
+	fcecfg.Set("controller","XINPUT_GAMEPAD_X", gamepad_x );
+	fcecfg.Set("controller","XINPUT_GAMEPAD_Y", gamepad_y);
+	fcecfg.Set("controller","XINPUT_GAMEPAD_LEFT_THUMB", gamepad_left_thumb );
+	fcecfg.Set("controller","XINPUT_GAMEPAD_RIGHT_THUMB", gamepad_right_thumb );
+	fcecfg.Set("controller","XINPUT_GAMEPAD_LEFT_SHOULDER", gamepad_left_shoulder);
+	fcecfg.Set("controller","XINPUT_GAMEPAD_RIGHT_SHOULDER", gamepad_right_shoulder);
+	fcecfg.Set("controller","XINPUT_LEFT_TRIGGER", gamepad_left_trigger);
+	fcecfg.Set("controller","XINPUT_RIGHT_TRIGGER", gamepad_right_trigger);
+	
 	//load the ini file
-	fcecfg.Load("game:\\fceui.ini");
-	fcecfg.Save("game:\\fceui.ini");
+	fcecfg.Load("game:\\fceui.ini");//overwrite default
+	fcecfg.Save("game:\\fceui.ini");//save in all case
 	
 }
 
