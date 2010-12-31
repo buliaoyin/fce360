414ca1c634f8eabcc3bc45973cff12b95df078ef
Ced2911 <Ced2911@1f2d827f-b39e-40f8-2521-43ba25b7e373>
2010-12-31 14:58:04 +0000

    Updated theme, removed some unused files

--- fce360/fceux/xbox/xbvideo.h
+++
@@ -1,8 +0,0 @@
-#include <xtl.h>
-
-void FCEUD_GetPalette(unsigned char i, unsigned char *r, unsigned char *g,
-        unsigned char *b) {
-    *r = pcpalette[i].r;
-    *g = pcpalette[i].g;
-    *b = pcpalette[i].b;
-}
