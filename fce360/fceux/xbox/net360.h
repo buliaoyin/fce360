fe388a88c18ea4c460f5aecb9c26bf6f13544e79
Ced2911 <Ced2911@1f2d827f-b39e-40f8-2521-43ba25b7e373>
2011-01-24 20:05:22 +0000

    Added start of network + various fixe

---
+++ fce360/fceux/xbox/net360.h
@@ -0,0 +1,6 @@
+int FCEUD_SendData(void *data, unsigned int len);
+int FCEUD_RecvData(void *data, unsigned int len);
+void FCEUD_NetworkClose(void);
+void FCEUD_NetworkOpen(void);
+
+int FCEUD_NetworkConnect(void);
